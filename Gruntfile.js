2c836039a67f3c76845b6f365554d3cd886a41bc
sagar176 <temp6735@gmail.com>
2024-11-19 17:58:40 +0530

    Changed version in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-const VISUAL_DEV_RELEASE_VERSION = '<%= package.version %>';
+const VISUAL_DEV_RELEASE_VERSION = '2504.0.0-rc1.0';
 const VISUAL_DEV_RELEASE_SPRINT = '<%= package.release %>';
 
-const WORKBOX_VERSION = '<%= package.devDependencies["workbox-build"] %>';
+const WORKBOX_VERSION = '6.5.4';
