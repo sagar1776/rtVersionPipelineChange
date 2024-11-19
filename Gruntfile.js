5ff67c0bed6d9dbdacce1bdd7af2d753a4f70271
sagar176 <temp6735@gmail.com>
2024-11-19 17:52:22 +0530

    Fixed workbox version in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
 const VISUAL_DEV_RELEASE_VERSION = '<%= package.version %>';
 const VISUAL_DEV_RELEASE_SPRINT = '<%= package.release %>';
 
-const WORKBOX_VERSION = '<%= package.release %>';
+const WORKBOX_VERSION = '<%= package.devDependencies["workbox-build"] %>';
